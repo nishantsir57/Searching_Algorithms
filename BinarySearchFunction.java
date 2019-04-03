441cbc4b79d83aa8fbde4ef11825cecfa00c0520
nishantsir57 <38082502+nishantsir57@users.noreply.github.com>
2019-04-03 10:23:41 +0530

    BinarySearchFunction.java

---
+++ BinarySearchFunction.java
@@ -0,0 +1,14 @@
+public static int binarySearch(int arr[], int element, int left, int right)
+	{
+	    if(right >= 0)
+	   {
+	   int mid=left+(right-1)/2;
+	   if(arr[mid] == element)
+	        return mid;
+	    if(arr[mid] > element)
+	        return binarySearch(arr, element, 0, mid-1);
+	    if(arr[mid] < element)
+	        return binarySearch(arr, element, mid+1, right);
+	   }
+	   return -1;
+	}

4fc13daa50d115556f6d7ec990b1cec5466e8a20
nishantsir57 <38082502+nishantsir57@users.noreply.github.com>
2019-04-03 09:58:03 +0530

    BinarySearch.java

---
+++ BinarySearch.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+class LinearSearch {
+public static int binarySearch(int arr[], int element, int left, int right)
+	{
+	    if(right >= 0)
+	   {
+	   int mid=left+(right-1)/2;
+	   if(arr[mid] == element)
+	        return mid;
+	    if(arr[mid] > element)
+	        return binarySearch(arr, element, 0, mid-1);
+	    if(arr[mid] < element)
+	        return binarySearch(arr, element, mid+1, right);
+	   }
+	   return -1;
+	}
+}
